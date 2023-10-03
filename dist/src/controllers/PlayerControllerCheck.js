c34ffbe89d6fd882f688a901b3265c72fa39711f
OllinDesigns <cesarvergara@tutanota.com>
2023-10-03 08:28:16 +0200

    commit with working post requests

--- dist/src/controllers/PlayerControllerCheck.js
+++ dist/src/controllers/PlayerControllerCheck.js
@@ -12,16 +12,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllPlayers = exports.createPlayer = void 0;
+exports.modifyPlayerName = exports.getAllPlayers = exports.createPlayer = void 0;
 const playerModelCheck_1 = __importDefault(require("../models/playerModelCheck"));
-// Function to handle creating a new player
 function createPlayer(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const { name, password } = req.body; // Adjust type as needed
+            let { name, password } = req.body; // Use 'let' instead of 'const'
+            // Check if the name is provided; if not, set it to "ANONYMOUS"
+            if (!name) {
+                name = "ANONYMOUS";
+            }
             const existingPlayer = yield playerModelCheck_1.default.findOne({ where: { name } });
             if (existingPlayer) {
-                return res.status(400).json({ error: "Player already exists, choose other name" });
+                return res
+                    .status(400)
+                    .json({ error: "Player already exists, choose another name" });
             }
             if (password.length < 6) {
                 return res
@@ -42,10 +47,41 @@ function createPlayer(req, res, next) {
     });
 }
 exports.createPlayer = createPlayer;
+// export async function createPlayer(
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) {
+//   try {
+//     const { name, password }: CreatePlayerRequest = req.body; // Adjust type as needed
+//     if (!name) {
+//       name = "ANONYMOUS";
+//     }
+//     const existingPlayer = await Player.findOne({ where: { name } });
+//     if (existingPlayer) {
+//       return res
+//         .status(400)
+//         .json({ error: "Player already exists, choose other name" });
+//     }
+//     if (password.length < 6) {
+//       return res
+//         .status(400)
+//         .json({ error: "Password should be at least 6 characters long" });
+//     }
+//     const newPlayer = await Player.create({ name, password });
+//     res
+//       .status(201)
+//       .json({ message: "Player created successfully", player: newPlayer });
+//     next();
+//   } catch (error) {
+//     console.error("Error creating player:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//     next(error);
+//   }
+// }
 function getAllPlayers(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            // Fetch all players from the database
             const players = yield playerModelCheck_1.default.findAll();
             res.status(200).json({ players });
         }
@@ -57,31 +93,28 @@ function getAllPlayers(req, res, next) {
     });
 }
 exports.getAllPlayers = getAllPlayers;
-// backup
-// import { Request, Response, NextFunction } from "express";
-// import Player from "../models/playerModelCheck";
-// // Function to handle creating a new player
-// export async function createPlayer(
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) {
-//   try {
-//     // Extract player data from the request body
-//     const { name, password }: any = req.body; // Adjust type as needed
-//     // Create a new player instance
-//     const newPlayer = await Player.create({ name, password });
-//     // Respond with a success message or the newly created player data
-//     res
-//       .status(201)
-//       .json({ message: "Player created successfully", player: newPlayer });
-//     // Call next to continue with the next middleware
-//     next();
-//   } catch (error) {
-//     // Handle errors, e.g., validation errors or database errors
-//     console.error("Error creating player:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//     // Call next with an error to trigger error handling middleware (if defined)
-//     next(error);
-//   }
-// }
+function modifyPlayerName(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const playerId = req.params.id; // Assuming you pass the player ID as a URL parameter
+            const { name } = req.body; // Adjust type as needed
+            // Check if the player with the given ID exists
+            const player = yield playerModelCheck_1.default.findOne({ where: { id: playerId } });
+            if (!player) {
+                return res.status(404).json({ error: "Player not found" });
+            }
+            // Update the player's name
+            player.name = name;
+            yield player.save();
+            res
+                .status(200)
+                .json({ message: "Player name updated successfully", player });
+        }
+        catch (error) {
+            console.error("Error modifying player name:", error);
+            res.status(500).json({ error: "Internal server error" });
+            next(error);
+        }
+    });
+}
+exports.modifyPlayerName = modifyPlayerName;
--- src/controllers/PlayerControllerCheck.ts
+++ src/controllers/PlayerControllerCheck.ts
@@ -1,19 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import Player from "../models/playerModelCheck";
-import { CreatePlayerRequest } from  "../types/playerTypes"
+import { CreatePlayerRequest } from "../types/playerTypes";
 
-// Function to handle creating a new player
 export async function createPlayer(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
-    const { name, password }: CreatePlayerRequest = req.body; // Adjust type as needed
+    let { name, password }: CreatePlayerRequest = req.body;
+
+    if (!name) {
+      name = "ANONYMOUS";
+    }
 
     const existingPlayer = await Player.findOne({ where: { name } });
     if (existingPlayer) {
-      return res.status(400).json({ error: "Player already exists, choose other name" });
+      return res
+        .status(400)
+        .json({ error: "Player already exists, choose another name" });
     }
 
     if (password.length < 6) {
@@ -43,7 +48,6 @@ export async function getAllPlayers(
   next: NextFunction
 ) {
   try {
-    // Fetch all players from the database
     const players = await Player.findAll();
 
     res.status(200).json({ players });
@@ -55,37 +59,70 @@ export async function getAllPlayers(
   }
 }
 
-// backup
+export async function modifyPlayerName(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const playerId = req.params.id;
+    const { name }: { name: string } = req.body;
 
-// import { Request, Response, NextFunction } from "express";
-// import Player from "../models/playerModelCheck";
+    const player = await Player.findOne({ where: { id: playerId } });
+    if (!player) {
+      return res.status(404).json({ error: "Player not found" });
+    }
 
-// // Function to handle creating a new player
+    player.name = name;
+    await player.save();
+
+    res
+      .status(200)
+      .json({ message: "Player name updated successfully", player });
+  } catch (error) {
+    console.error("Error modifying player name:", error);
+    res.status(500).json({ error: "Internal server error" });
+
+    next(error);
+  }
+}
+
 // export async function createPlayer(
 //   req: Request,
 //   res: Response,
 //   next: NextFunction
 // ) {
 //   try {
-//     // Extract player data from the request body
-//     const { name, password }: any = req.body; // Adjust type as needed
+//     const { name, password }: CreatePlayerRequest = req.body; // Adjust type as needed
 
-//     // Create a new player instance
+//     if (!name) {
+//       name = "ANONYMOUS";
+//     }
+
+//     const existingPlayer = await Player.findOne({ where: { name } });
+//     if (existingPlayer) {
+//       return res
+//         .status(400)
+//         .json({ error: "Player already exists, choose other name" });
+//     }
+
+//     if (password.length < 6) {
+//       return res
+//         .status(400)
+//         .json({ error: "Password should be at least 6 characters long" });
+//     }
+
 //     const newPlayer = await Player.create({ name, password });
 
-//     // Respond with a success message or the newly created player data
 //     res
 //       .status(201)
 //       .json({ message: "Player created successfully", player: newPlayer });
 
-//     // Call next to continue with the next middleware
 //     next();
 //   } catch (error) {
-//     // Handle errors, e.g., validation errors or database errors
 //     console.error("Error creating player:", error);
 //     res.status(500).json({ error: "Internal server error" });
 
-//     // Call next with an error to trigger error handling middleware (if defined)
 //     next(error);
 //   }
 // }
